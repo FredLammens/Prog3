f3556d8c98166ce8f86e8467e62df9e321c8ebd2
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-02-19 09:04:45 +0100

    geprutst met DirInfo

--- FileIO/Program.cs
+++ FileIO/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.IO;
 namespace FileIO
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("**** Fun with Directory(info) ****\n");
+            ShowWindowsDirectoryInfo();
+            Console.ReadLine();
+        }
+
+        private static void ShowWindowsDirectoryInfo()
+        {
+            //Dump directory information.
+            DirectoryInfo dir = new DirectoryInfo(@"C:\"); // the @ is to escape the slashes
+            DirectoryInfo[] downloadsFolder = dir.GetDirectories("*downloads*"); // zelf testen
+            Console.WriteLine("**** Directory Info ****");
+            Console.WriteLine("Fullname: {0}",dir.FullName);
+            Console.WriteLine("Name: {0}",dir.Name);
+            Console.WriteLine("Parent: {0}",dir.Parent);
+            Console.WriteLine("Creation: {0}",dir.CreationTime);
+            Console.WriteLine("Attributes : {0}",dir.Attributes);
+            Console.WriteLine("Root: {0}",dir.Root);
+            Console.WriteLine("************************");
         }
     }
 }
