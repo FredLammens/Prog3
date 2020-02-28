6e159feceb327b62165e5e44948048386c5f20b9
Frederic Lammens <frederic.lammens@student.hogent.be>
2020-02-28 10:29:31 +0100

    added select cursu oef

---
+++ LINQ/SelectCursOef.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ
+{
+    class SelectCursOef
+    {
+        public static void Select1(IList<Program.Student> studentList) 
+        {
+            Console.WriteLine("---------Select 1 ---------");
+            var sel = studentList.Select(s => s.StudentName);
+            foreach (var x in sel) Console.WriteLine(x);
+            Console.WriteLine("---------------");
+        }
+        public static void Select2(IList<Program.Student> studentList) 
+        {
+            Console.WriteLine("----------Select 2 ------------");
+            var sel = studentList.Select(s => new { naam = s.StudentName, aantalCursussen = s.cursussen.Count() });
+            foreach (var x in sel) Console.WriteLine(x);
+            Console.WriteLine("------------------");
+        }
+        public static void Select3(IList<Program.Student> studentList) 
+        {
+            Console.WriteLine("-----------Select 3 -------------");
+            var sel = studentList.SelectMany(s => s.cursussen);
+            foreach (var x in sel) Console.WriteLine(x);
+            Console.WriteLine("----------------");
+        }
+        public static void Select4(IList<Program.Student> studentList) 
+        {
+            Console.WriteLine("--------------Select 4 -------------");
+            var sel = studentList.SelectMany(s => s.cursussen).Distinct();
+            foreach (var x in sel) Console.WriteLine(x);
+            Console.WriteLine("------------------");
+        }
+        public static void Select5(IList<Program.Student> studentList) 
+        {
+            Console.WriteLine("--------------Select 5--------------");
+            var sel = studentList.SelectMany(s => s.cursussen,
+                (student, program) => new
+                {
+                    studentName = student.StudentName,
+                    cursusName = program
+                });
+            foreach (var x in sel) Console.WriteLine(x);
+            Console.WriteLine("--------------------");
+        }
+    }
+}
